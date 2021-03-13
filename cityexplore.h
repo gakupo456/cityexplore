17ca888bd79b30cf1d001d506eaea8c9f02f2543
Tomás Ayala <56085978+gakupo456@users.noreply.github.com>
2021-03-13 22:59:55 +0100

    Add files via upload

--- cityexplore.h
+++ cityexplore.h
@@ -1,5 +1,9 @@
+#ifndef CITYEXPLORE_H_
+#define CITYEXPLORE_H_
+
 #include "FL/Fl_Text_Buffer.H"
 #include "FL/Fl_File_Chooser.H"
+#include "FL/Fl_Text_Editor.H"
 #include "FL/Fl_Text_Display.H"
 #include "FL/Fl_Tabs.H"
 #include "FL/Fl_Input_Choice.H"
@@ -11,3 +15,7 @@
 #include "FL/Fl_File_Icon.H"
 #include "FL/Fl_Light_Button.H"
 #include "FL/Fl_Double_Window.H"
+#include "FL/Fl_Multiline_Output.H"
+
+
+#endif
